2e7a551f83e06d432f9257ad096ade0f20f907d2
feffy380 <114889020+feffy380@users.noreply.github.com>
2023-05-13 02:56:09 +0200

    refactor model classes

--- MLP.py
+++ MLP.py
@@ -27,23 +27,22 @@ class MLP(pl.LightningModule):
     def forward(self, x):
         return self.layers(x)
 
-    def training_step(self, batch, batch_idx):
+    def compute_loss(self, batch):
         x = batch[0]
         y = batch[1].reshape(-1, 1)
         x_hat = self.layers(x)
         loss = F.mse_loss(x_hat, y)
+        return loss
 
+    def training_step(self, batch, batch_idx):
+        loss = self.compute_loss(batch)
         self.log(
             "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
         )
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x = batch[0]
-        y = batch[1].reshape(-1, 1)
-        x_hat = self.layers(x)
-        loss = F.mse_loss(x_hat, y)
-
+        loss = self.compute_loss(batch)
         self.log("val_loss", loss, prog_bar=True, logger=True)
         return loss
 
@@ -59,49 +58,31 @@ class MLP(pl.LightningModule):
 
 
 class ILModel(MLP):
-    def training_step(self, batch, batch_idx):
+    """
+    Irreducible loss model for RHO-Loss
+    """
+
+    def compute_loss(self, batch):
         idx, x, y = batch
-        y = y.reshape(-1, 1)
-        x_hat = self.layers(x)
-        loss = F.mse_loss(x_hat, y)
-
-        self.log(
-            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        )
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        idx, x, y = batch
-        y = y.reshape(-1, 1)
-        x_hat = self.layers(x)
-        loss = F.mse_loss(x_hat, y)
-
-        self.log("val_loss", loss, prog_bar=True, logger=True)
+        loss = super().compute_loss((x, y))
         return loss
 
 
 class RLossModel(MLP):
+    """
+    Reducible loss model for RHO-Loss
+    """
+
     def __init__(self, input_size, lr=1e-3, selection_method=None):
         super().__init__(input_size, lr)
         self.selection_method = selection_method
 
-    def training_step(self, batch, batch_idx):
-        batch = self.selection_method(batch=batch, model=self, loss_function=F.mse_loss)
+    def compute_loss(self, batch):
         idx, x, y = batch
-        y = y.reshape(-1, 1)
-        x_hat = self.layers(x)
-        loss = F.mse_loss(x_hat, y)
-
-        self.log(
-            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        )
+        loss = super().compute_loss((x, y))
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        idx, x, y = batch
-        y = y.reshape(-1, 1)
-        x_hat = self.layers(x)
-        loss = F.mse_loss(x_hat, y)
-
-        self.log("val_loss", loss, prog_bar=True, logger=True)
+    def training_step(self, batch, batch_idx):
+        batch = self.selection_method(batch=batch, model=self, loss_function=F.mse_loss)
+        loss = super().training_step(batch, batch_idx)
         return loss
